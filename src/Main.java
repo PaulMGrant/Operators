9341f747e5861beb1615adb902b468a4795490a5
Paul M Grant <paul@paul-grant.co.uk>
2023-02-22 15:13:35 +0100

    Ver 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int result = 1 + 2; //1+2=3
+        System.out.println("1 + 2 = " + result);
+        int previousResult = result;
+        System.out.println("previousResult = " + previousResult);
+        result = result - 1;  //3-1=2
+        System.out.println("3-1 = " + result);
+        System.out.println("previousResult = " + previousResult);
+
+        result = result *10; //2*10=20
+        System.out.println("Result x 10 = " + result);
+
     }
 }
