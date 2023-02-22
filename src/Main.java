c0244efbc23cee2a5370f96f311051c3a953936a
Paul M Grant <paul@paul-grant.co.uk>
2023-02-22 15:55:49 +0100

    and operator

--- src/Main.java
+++ src/Main.java
@@ -45,5 +45,15 @@ public class Main {
             System.out.println("and I am scared of aliens");
         }
 
+        int topScore = 80;
+        if (topScore == 100) { //!= - not equal to > - greater than
+            System.out.println("You got the high score");
+        }
+        int secondTopScore = 81;
+        if(topScore > secondTopScore && topScore < 100){
+            System.out.println("Greater than second top score and less than 100");
+        }
+
+
     }
 }
